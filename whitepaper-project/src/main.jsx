7e0615f7f84e45b5cfc9784dae2761edb07c53b2
lmpollockmuguruza <lucas-pollock@outlook.com>
2026-02-25 20:47:20 +0000

    Delete whitepaper-project directory

--- whitepaper-project/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import WhitePaper from './WhitePaper'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <WhitePaper />
-  </React.StrictMode>
-)
